1a7286bd62f324657ca93770685f6b31055e00f7
康 梁 <1927810337@qq.com>
2018-07-12 00:49:45 +0800

    添加项目文件。

---
+++ Project1/set.cpp
@@ -0,0 +1,31 @@
+#include<bits\stdc++.h>
+using namespace std;
+void print(set<int> s) {
+	set<int>::iterator it;
+	for (it = s.begin(); it != s.end(); it++) {
+		cout << *it << " ";
+	}
+	printf("\n");
+}
+
+int main() {
+	set<int> s;
+	s.insert(1);
+	s.insert(3);
+	s.insert(5);
+	s.insert(9);
+	s.insert(4);
+	print(s);
+	s.insert(0);
+	print(s);
+	s.clear();
+	print(s);
+	for (int i = 9; i >= 0; i--) {
+		s.insert(i);
+	}
+	print(s);
+	s.erase(s.begin());
+	print(s);
+	cout << (s.find(20) == s.end()) << endl;
+	return 0;
+}
---
+++ Project1/vector.cpp
@@ -0,0 +1,31 @@
+//#include<bits\stdc++.h>
+//using namespace std;
+//void print(vector<int> v) {
+//	vector<int>::iterator it;
+//	for (it = v.begin(); it != v.end(); it++) {
+//		cout << *it<<" ";
+//	}
+//	printf("\n");
+//}
+//void printf(vector<int> v) {
+//	for (int i = 0; i <= v.size() - 1; i++) {
+//		cout << v[i]<<" ";
+//	}
+//	printf("\n");
+//}
+//int main() {
+//	//¶¨ÒåvectorÈÝÆ÷
+//	vector<int> v;
+//	vector<int>::iterator it;
+//	for (int i = 0; i < 10; i++) {
+//		v.push_back(i);
+//	}
+//	print(v);
+//	v.pop_back();
+//	print(v);
+//	v.insert(v.begin(), 4, 3);
+//	print(v);
+//	v.erase(v.begin() + 2);
+//	printf(v);	
+//	
+//}
